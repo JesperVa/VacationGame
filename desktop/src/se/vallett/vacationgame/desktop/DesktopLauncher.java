4ea62920ae60d91e4fd4a83173c99322029e0a10
Jesper Vallett <smoovefan@hotmail.se>
2018-12-09 19:32:04 +0100

    Added build dependencies
    Initalised the LibGDX project and added Lombdok build dependencies to
    gradle.

---
+++ desktop/src/se/vallett/vacationgame/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package se.vallett.vacationgame.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import se.vallett.vacationgame.VacationGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new VacationGame(), config);
+	}
+}
